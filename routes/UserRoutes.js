e4281df932523e032fc524c4adff931c82a7eaa8
danysalame18 <65205086+danysalame18@users.noreply.github.com>
2020-08-16 21:34:07 -0500

    actualizacion

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,9 +5,9 @@ const { UserController } = require('../controllers')
 const { UserValidator } = require('../validators')
 const { verifyToken }= require('../middlewares')
 
-router.get('/users', verifyToken, UserController.findAll);
+router.get('/users', UserController.findAll);
 router.get('/users/:id', verifyToken, UserController.findOne);
-router.post('/users', verifyToken, UserValidator.create, UserController.create);
+router.post('/users', UserValidator.create, UserController.create);
 router.patch('/users/:id', verifyToken, UserValidator.edit, UserController.edit);
 router.delete('/users/:id', verifyToken, UserController.eliminate)
 
